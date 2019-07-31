14a0fccf63588566e501281370bfc2da8f5ef538
xuyun <xuyun@windimg.com>
2019-07-31 15:03:50 +0800

    minimum-window-substring

--- minimum-window-substring/main.go
+++ minimum-window-substring/main.go
@@ -2,7 +2,68 @@ package minimum_window_substring
 
 func minWindow(s string, t string) string {
 
-	var tmap = make(map[string]int, 10)
-	var smap = make(map[string]int, 10)
+	var tmap = make(map[string]int, 10) //记录t中的字符以及个数
+	var smap = make(map[string]int, 10) //记录window中命中t的字符个数
+
+	var begin, end int //记录window的起始位置
+	var count int      // 记录window中符合条件的字符个数
+
+	var found = false
+	var result = s
+
+	//初始化tmap
+	for i := 0; i < len(t); i++ {
+		cur := t[i : i+1]
+		if _, ok := tmap[cur]; ok {
+			tmap[cur]++
+		} else {
+			tmap[cur] = 1
+		}
+	}
+
+	for end < len(s) {
+
+		//移动end，直到窗口符合条件
+		for count < len(t) && end < len(s) {
+			cur := s[end : end+1]
+			val := 1
+			if tv, ok := smap[cur]; ok {
+				smap[cur]++
+				val = tv + 1
+			} else {
+				smap[cur] = 1
+			}
+			//符合条件，count++
+			if tmap[cur] >= val {
+				count++
+			}
+			end++
+		}
+
+		//找到了窗口，开始移动begin
+		for count == len(t) && begin < end {
+			found = true
+			if end-begin < len(result) {
+				result = s[begin:end]
+			}
+
+			cur := s[begin : begin+1]
+			val := smap[cur] - 1
+			smap[cur]--
+			if tval, ok := tmap[cur]; ok {
+				//如果t中存在这个字符，且个数比窗口中的大，则count--
+				if tval > val {
+					count--
+				}
+			}
+			begin++
+		}
+
+	}
+
+	if !found {
+		return ""
+	}
+	return result
 
 }
--- minimum-window-substring/main_test.go
+++ minimum-window-substring/main_test.go
@@ -7,11 +7,43 @@ func Test_minWindow(t *testing.T) {
 		s string
 		t string
 	}
-	var tests []struct {
+	tests := []struct {
 		name string
 		args args
 		want string
+	}{
+		{
+			"case1",
+			args{"ABCAAABEFD", "AD"},
+			"ABEFD",
+		},
+		{
+			"case2",
+			args{"AD", "AD"},
+			"AD",
+		},
+		{
+			"case3",
+			args{"ABCAAABEFD", "D"},
+			"D",
+		},
+		{
+			"case4",
+			args{"ABCAAA", "ACB"},
+			"ABC",
+		},
+		{
+			"case5",
+			args{"ABCAAABEFD", "AACBD"},
+			"CAAABEFD",
+		},
+		{
+			"case6",
+			args{"ABCAAABFD", "AE"},
+			"",
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := minWindow(tt.args.s, tt.args.t); got != tt.want {
