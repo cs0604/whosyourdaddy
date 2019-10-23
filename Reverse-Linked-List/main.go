6183ff7561a8a27570084fa923f0df384acaf5d2
xuyun <xuyun@windimg.com>
2019-10-23 22:21:25 +0800

    111

---
+++ Reverse-Linked-List/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+//Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+
+	if head == nil {
+		return nil
+	}
+	ret := reverse(head)
+	head.Next = nil
+	return ret
+}
+
+func reverse(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return head
+	}
+	p := head.Next
+	ret := reverseList(p)
+	p.Next = head
+	return ret
+}
+
+func lprint(head *ListNode) {
+	for q := head; q != nil; q = q.Next {
+		fmt.Print(q.Val, "->")
+	}
+	fmt.Println()
+}
+
+func main() {
+	head := &ListNode{Val: 0}
+	p := head
+	for i := 1; i < 5; i++ {
+
+		p.Next = &ListNode{Val: i}
+		p = p.Next
+
+	}
+
+	lprint(head)
+
+	head = reverseList(head)
+
+	lprint(head)
+
+}
