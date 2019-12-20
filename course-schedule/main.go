a49ebc44767724774aa7dbcc213baac850aa0653
xuyun <xuyun@windimg.com>
2019-12-20 12:52:20 +0800

    优化一下速度

--- course-schedule/main.go
+++ course-schedule/main.go
@@ -2,32 +2,42 @@ package course_schedule
 
 func canFinish(numCourses int, prerequisites [][]int) bool {
 
-	var invalid = make([][]bool, numCourses)
-	for i := 0; i < numCourses; i++ {
-		invalid[i] = make([]bool, numCourses)
-	}
+	var indegree = make([]int, numCourses)
 
+	var graph = make([][]int, numCourses)
+
+	//build graph and indegree
 	for i := 0; i < len(prerequisites); i++ {
 		x := prerequisites[i][0]
 		y := prerequisites[i][1]
-
-		if invalid[x][y] {
-			return false
-		}
-
-		invalid[y][x] = true
-
-		for j := 0; j < numCourses; j++ {
-			if invalid[x][j] {
-				if j == y {
-					return false
-				}
-				invalid[y][j] = true
-			}
-		}
-
+		indegree[x]++
+		graph[y] = append(graph[y], x)
 	}
 
-	return true
+	//每次从graph中找一个indegree为0的节点开始处理，找不到就说明有环
+	//bfs
+	var queue []int
+	for i := 0; i < numCourses; i++ {
+		if indegree[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
+
+	visit := 0
+
+	for len(queue) > 0 {
+		cur := queue[0]
+		visit++
+		queue = queue[1:]
+		for j := 0; j < len(graph[cur]); j++ {
+			pos := graph[cur][j]
+			indegree[pos]--
+			if indegree[pos] == 0 {
+				queue = append(queue, pos)
+			}
+		}
+	}
+
+	return visit == numCourses
 
 }
